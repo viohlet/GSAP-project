369bba475e0e6a6d72b98a53492fd7fe3132066b
Violeta Venegas <violetavenegas@hotmail.com>
2019-01-13 22:55:02 -0500

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# GSAP-project
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+import { TweenMax } from "gsap";
+// import { TimeLineMax } from "gsap";
+// import { TweenMax, Draggable } from "gsap/all";
+
+document.getElementById("app").innerHTML = `
+      
+<div id="content">
+    <img id="generated" src="img/generated.svg" alt="generated" />
+  <h1>Hello Creative Tech Team!</h1>
+  <p>You can check out my design portfolio and CV
+  <a href="https://violetavenegas6.wixsite.com/site" target="_blank" rel="noopener noreferrer">here</a>
+  </p>
+  </div>
+</div>
+
+<div id="star"> </div>
+
+`;
+
+TweenMax.from("#generated", 1, { scale: 0, ease: Bounce.easeOut });
+// timeline.from("h1", 4, { opacity: 0 });
+TweenMax.from(
+  "h1",
+  1,
+  { opacity: 0, scale: 1, x: 90, ease: Back.easeOut.config(3), delay: 1 },
+  0.1
+);
+TweenMax.from(
+  "p",
+  1,
+  { opacity: 0, scale: 1, x: -90, ease: Back.easeOut.config(3), delay: 1 },
+  0.1
+);
+
+TweenMax.fromTo(
+  "#star",
+  1,
+  {
+    rotation: 40
+  },
+  {
+    rotation: -40,
+    delay: 1,
+    repeat: -1,
+    repeatDelay: 1,
+    yoyo: true
+  }
+);
